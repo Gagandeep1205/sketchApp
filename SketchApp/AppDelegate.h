d575ec6d9e6d8fb8eff0a94255f06d4e0148dc93
Kunal Gupta <kunalgupta@Kunals-Mac-mini.local>
2015-08-10 12:08:13 +0530

    Initial Commit

---
+++ SketchApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SketchApp
+//
+//  Created by Kunal Gupta on 10/08/15.
+//  Copyright (c) 2015 Kunal Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
